d8548c746b471fdd1bb03161688cdb03afa584e9
kirubahari76 <93319293+kirubahari76@users.noreply.github.com>
2021-11-06 12:57:07 +0530

    Add files via upload

---
+++ exe5.rb
@@ -0,0 +1,17 @@
+my_name = ' Kirubakaran' #my father called Kiruba
+my_age = 22 # Y.O.B = 1999
+my_height = 6.5 #feet
+my_weight = 70 # kg
+my_eyes = ' Brown'
+my_hair = ' Black'
+my_skin = ' Black '
+
+puts " Interviewer asked Kiruba to tell about yourself "
+puts " what kiruba said is:"
+puts " I'm #{my_name} "
+puts " I'm #{my_age} years old "
+puts " I'm #{my_weight} kilograms heavy"
+puts "my idebntifications are:"
+puts " my eye's are #{my_eyes}, my skin and hair is #{my_hair} in colour "
+puts " I wont add the #{my_age}, #{my_height} and #{my_weight}. Beacause this doesnt make sense"
+puts " but the value is #{my_age + my_height + my_weight}"
