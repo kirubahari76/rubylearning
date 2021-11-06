0b3486cf984b8c733d1cd699c7367444c8d736dd
kirubahari76 <93319293+kirubahari76@users.noreply.github.com>
2021-11-06 12:52:24 +0530

    Add files via upload
    Code for exercise 1

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+#puts "Hello World"
+puts "Hello Again"
+puts "I LIke Typing thiS"
+puts "This is fun"
+puts "Yay! Printing."
+puts 'I "said" do not touch this.'
+puts "I'd much rather you 'not'."
+puts "puts actually does printing what's inside the double quotes("") or single quotes('')"
