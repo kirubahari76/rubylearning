ed5b938c2daed4a7cd56f8a2e75010b96c6948da
kirubahari76 <93319293+kirubahari76@users.noreply.github.com>
2021-11-08 12:58:45 +0530

    Add files via upload

---
+++ exe9.rb
@@ -0,0 +1,12 @@
+days = " sunday monday tuesday wednesday thursday friday saturday"
+months = "\njanuary \nfebruary \nmarch \napril \nmay "
+puts "Here are the days of the week: #{days}"
+puts  "Here the first 5 month of the year : #{months}"
+#\n move to the next line
+
+puts %q{ Im kirubakaran,
+my favourite hobby is dancing
+dancing enhance the soul of every human life}
+#%q converts entire filling into strings
+#format for the quotes is %q{}
+#there should be no space bw the %q and the {}  %q{}
