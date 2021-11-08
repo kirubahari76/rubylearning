a437d072123e584bb35788de3caaf9c436264028
kirubahari76 <93319293+kirubahari76@users.noreply.github.com>
2021-11-08 15:33:04 +0530

    Add files via upload

---
+++ 11exe.rb
@@ -0,0 +1,11 @@
+#How to get the inputs from the user
+puts " Whats your name "
+name = gets.chomp
+print "whats your educational qualification "
+edu_qualification = gets.chomp
+print 'where have u been completed your UG '
+UG_institute = gets.chomp
+
+puts " So your name is #{name} , you have completed #{edu_qualification} in #{UG_institute}"
+
+print " \t\n*#{name} \t\n*#{edu_qualification} \t\n*#{UG_institute}"
