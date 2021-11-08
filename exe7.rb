5c422076313f4e5e08958ccf83ddfe48b4375097
kirubahari76 <93319293+kirubahari76@users.noreply.github.com>
2021-11-08 10:47:59 +0530

    Add files via upload

---
+++ exe7.rb
@@ -0,0 +1,27 @@
+no_of_vehicles = 4
+cars = 2
+bike = no_of_vehicles - cars #to count the bikes
+puts " Kiruba have a #{bike} motorcycles"
+first_car_colour = " white"
+puts "one bike is #{first_car_colour} in colour and the second one is #{'red'} in colour"
+puts "." *60
+
+end1 = "K"
+end2 = "I"
+end3 = "L"
+end4 = "L"
+end5 = "E"
+end6 = "r"
+print end1 + end2 + end3 + end4 + end5
+puts end6
+#comparison between puts vs print
+puts end1 + end2
+print end3 + end4
+
+#print vs print
+print end1 + end2
+print end3 + end4
+
+#print vs puts
+print end1 + end2
+puts end3 + end4
