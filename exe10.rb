a9c390ffd13e40d2ac5621201e26ec8abfaff6f3
kirubahari76 <93319293+kirubahari76@users.noreply.github.com>
2021-11-08 14:36:35 +0530

    Add files via upload

---
+++ exe10.rb
@@ -0,0 +1,19 @@
+#How to escape from the error of missing quotes
+puts "My house is 4'66\" in location"
+puts 'I\'m Kirubakaran'
+#let's plY WITH backslash
+tabber = " \tit moves one tab from current line"
+puts tabber
+splitter = " the text is going to split into the \nnew line"
+puts splitter
+backslasher = " It avoids the first \\back \\slash"
+puts backslasher
+
+personal_details = """
+some details of the student:
+\t* Kirubakaran\n
+\t* B.E\n
+\t* PSG\n \n\t* Coimbatore\n
+
+"""
+puts personal_details
