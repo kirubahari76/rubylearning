b62f5c81cdb30273a963688588ee5f9e6f99bea3
kirubahari76 <93319293+kirubahari76@users.noreply.github.com>
2021-11-08 10:19:25 +0530

    Add files via upload

---
+++ exe6.rb
@@ -0,0 +1,17 @@
+
+no_of_friends = 4
+
+k = " I have #{no_of_friends} friends"
+friends = "frds"
+have = "hv"
+y = " My #{friends} dont #{have} any kind of attitude"
+
+puts k
+puts y
+
+ puts " I said #{k}"
+ puts "I also said #{y}"
+
+ lo = " Kiruba "
+ ve = " karan "
+ puts lo+ve
